babbeb286b38313cee183309c014ca9463ffd77f
Vasiliy <wudgexyz@gmail.com>
2023-10-02 20:57:24 +0300

    binary search

---
+++ src/search/BinarySearch.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace search
+{
+    internal class BinarySearch
+    {
+
+        public int Recursive(int[] array, int digit, int startIndex, int lastIndex)
+        {
+            if (startIndex > lastIndex)
+            {
+                return -1;
+            }
+            int middleIndex = (startIndex + lastIndex) / 2;
+            int middleValue = array[middleIndex];
+            if (middleValue == digit)
+            {
+                return middleIndex;
+            }
+            if (middleValue > digit)
+            {
+                return Recursive(array, digit, startIndex, middleIndex - 1);
+            }
+            else
+            {
+                return Recursive(array, digit, middleIndex + 1, lastIndex);
+            }
+        }
+        public int Iterative(int[] array, int digit)
+        {
+            int left = 0, right = array.Length - 1;
+            while (left <= right)
+            {
+                int middleIndex = (left + right) / 2;
+                if (array[middleIndex] == digit)
+                {
+                    return middleIndex;
+                }
+                else if (digit < array[middleIndex])
+                {
+                    right = middleIndex - 1;
+                }
+                else { left = middleIndex + 1; }
+            }
+            return -1;
+        }
+
+
+        public int Default(int[] array, int digit)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == digit)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+    }
+}
---
+++ src/search/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+namespace search
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string url = "https://www.dropbox.com/s/im722je8vi6xzki/app-debug.apk?dl=1";
+            DownloadFileAsync(url).Wait();
+
+            Console.WriteLine("Загрузка завершена.");
+            Console.ReadLine();
+        }
+
+        private static async Task DownloadFileAsync(string url)
+        {
+            using (var client = new WebClient())
+            {
+                // Включаем события прогресса скачивания
+                client.DownloadProgressChanged += (sender, e) =>
+                {
+                    Console.Write($"\rСкачивание: {e.ProgressPercentage}% ({e.BytesReceived}/{e.TotalBytesToReceive} байт)");
+                };
+
+                // Включаем событие завершения скачивания
+                client.DownloadFileCompleted += (sender, e) =>
+                {
+                    Console.WriteLine($"\nСкачивание файла завершено.");
+                };
+
+                // Запускаем асинхронную загрузку файла
+                await client.DownloadFileTaskAsync(new Uri(url), "file.txt");
+            }
+        }
+    }
+}
